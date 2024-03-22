b6f7c992f0fbc78b955c80fa6bdf08a8ad4fc00d
Lara Rossetti <lara.rossetti25@gmail.com>
2024-03-21 22:03:52 -0300

    Implementar resposta do verificador de idade e sexo

--- aula 12 exec/script.js
+++ aula 12 exec/script.js
@@ -1,14 +1,25 @@
 function Verificar(){
     var anot = window.document.getElementById('txtano')
-    var sex = window.document.getElementById('sexo')
+    var sex = window.document.querySelector('input[name=txtsex]:checked').value
     var frase = window.document.getElementById('frase')
+    var year = new Date().getFullYear()
+    
+    var maiordeidade = (anodenascimento) => {
+        return anodenascimento <= (year-18)
+    }
 
     var ano = Number(anot.value)
 
-    if(ano <= '2006'){
-        frase.innerText = 'Você é maior de idade.'
-        
-    } else{
-        frase.innerText = 'Você é menor de idade.'
+    if(maiordeidade(ano) && sex == 'Feminino'){
+        frase.innerText = 'Você é uma mulher maior de idade.'
+    } else if(maiordeidade(ano) && sex == 'Masculino'){
+        frase.innerText = 'Você é um homem maior de idade.'
+    } else if(!maiordeidade(ano) && sex == 'Feminino'){
+        frase.innerText = 'Você é uma mulher menor de idade.'
+    } else if(!maiordeidade(ano) && sex == 'Masculino'){
+        frase.innerText = 'Você é um homem menor de idade.'
     }
+
+  
+    
 }
