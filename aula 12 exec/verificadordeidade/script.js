977e3fe0b80195dd3e6ede6a052b7f48b9b20d24
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-02 14:49:12 -0300

    Adicionando imagem a cada idade

--- aula 12 exec/verificadordeidade/script.js
+++ aula 12 exec/verificadordeidade/script.js
@@ -1,26 +1,68 @@
 function Verificar(){
     var anot = window.document.getElementById('txtano')
-    var sex = window.document.querySelector('input[name=txtsex]:checked').value
+    var sex = window.document.getElementsByName('txtsex')
     var frase = window.document.getElementById('frase')
     var year = new Date().getFullYear()
+    var gênero = ''
+    var idade = year - Number(anot.value)
+   // var faixaetaria = ''
+
     
-    var maiordeidade = (anodenascimento) => {
-        return anodenascimento <= (year-18)
-    }
-
-
+    // var img = window.document.getElementById('imagem')
+    
+   // var maiordeidade = (anodenascimento) => {
+  //      return anodenascimento <= (year-18)
+   // }
+    
   
-    var ano = Number(anot.value)
+    
+    /*
+    
+        if(idade >= 0 && idade <= 2){
+            faixaetaria = 'bebê'
+        }else if(idade <= 12){
+            faixaetaria = 'criança'
+        }else if(idade <= 21){
+            faixaetaria = 'adolescente'
+        }else if(idade <= 60){
+            faixaetaria = 'adulto'
+        }else if(idade <= 110){
+            faixaetaria = 'idoso'
+        }
+        
+    /*
+  
 
-    if(maiordeidade(ano) && sex == 'Feminino'){
+
+
+
+/*
+    if(maiordeidade(ano) && gênero == 'Mulher'){
         frase.innerText = `Você é uma mulher de ${ano} anos.`
+       // img.src = 'adolescente feminina.jpg'
     } else if(maiordeidade(ano) && sex == 'Masculino'){
         frase.innerText = 'Você é um homem maior de idade.'
     } else if(!maiordeidade(ano) && sex == 'Feminino'){
         frase.innerText = 'Você é uma mulher menor de idade.'
     } else if(!maiordeidade(ano) && sex == 'Masculino'){
         frase.innerText = 'Você é um homem menor de idade.'
-    }
+    }*/
 
     
+    if(sex[0].checked){
+        gênero = 'Masculino'
+    }else if(sex[1].checked){
+        gênero = 'Feminino'
+    }
+
+    if(gênero == 'Feminino'){
+        frase.innerText = `Detectamos uma Mulher com ${idade} anos.`
+    }else{
+        frase.innerText = `Detectamos um Homem com ${idade} anos.`
+    }
+    
+    
+
+
+
 }
