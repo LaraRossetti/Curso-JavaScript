032b253b25e8c9eaf9178108669b21ae089f0099
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-02 21:32:03 -0300

    Adicionar verificação de dados

--- aula 12 exec/verificadordeidade/script.js
+++ aula 12 exec/verificadordeidade/script.js
@@ -7,45 +7,54 @@ function Verificar(){
     var idade = year - Number(anot.value)
 
     var img = window.document.getElementById('imagem')
-  
-    if(sex[0].checked){
-        gênero = 'Masculino'
-        if(idade >= 0 && idade <= 2){
-            img.setAttribute('src','bb masculino.png')
-        }else if(idade <= 10){
-            img.setAttribute('src','criança masculino.png')
-        }else if(idade <= 21){
-            img.setAttribute('src','adolescente masculino.png')
-        }else if(idade <= 60){
-            img.setAttribute('src','adulto masculino.png')
-        }else if(idade <= 110){
-            img.setAttribute('src','idoso masculino.png')
-        }
-    }else if(sex[1].checked){
-        gênero = 'Feminino'
-        if(idade >= 0 && idade <= 2){
-            img.setAttribute('src','bb feminino.png')
-        }else if(idade <= 10){
-            img.setAttribute('src','criança feminino.png')
-        }else if(idade <= 21){
-            img.setAttribute('src','adolescente feminino.png')
-        }else if(idade <= 60){
-            img.setAttribute('src','adulta feminina.png')
-        }else if(idade <= 110){
-            img.setAttribute('src','idosa feminina.png')
-        }
-    }
-
-    if(gênero == 'Feminino'){
-        frase.innerText = `Detectamos uma Mulher com ${idade} anos.`
-        frase.appendChild(img)
-    }else{
-        frase.innerText = `Detectamos um Homem com ${idade} anos.`
-        frase.appendChild(img)
-    }
-    
     
 
 
+    if(idade < 0 || idade > 110){
+        window.alert('Verifique seus dados e tente novamente.')
+    }
+    else{
+        if(sex[0].checked){
+            gênero = 'Masculino'
+            if(idade >= 0 && idade <= 2){
+                img.setAttribute('src','bb masculino.png')
+            }else if(idade <= 10){
+                img.setAttribute('src','criança masculino.png')
+            }else if(idade <= 21){
+                img.setAttribute('src','adolescente masculino.png')
+            }else if(idade <= 60){
+                img.setAttribute('src','adulto masculino.png')
+            }else{
+                img.setAttribute('src','idoso masculino.png')
+            }
+        }else if(sex[1].checked){
+            gênero = 'Feminino'
+            if(idade >= 0 && idade <= 2){
+                img.setAttribute('src','bb feminino.png')
+            }else if(idade <= 10){
+                img.setAttribute('src','criança feminino.png')
+            }else if(idade <= 21){
+                img.setAttribute('src','adolescente feminino.png')
+            }else if(idade <= 60){
+                img.setAttribute('src','adulta feminina.png')
+            }else{
+                img.setAttribute('src','idosa feminina.png')
+            }
+        }   
+   
 
+
+        if(gênero == 'Feminino'){
+            frase.innerText = `Detectamos uma Mulher com ${idade} anos.`
+            frase.appendChild(img)
+        }else{
+            frase.innerText = `Detectamos um Homem com ${idade} anos.`
+            frase.appendChild(img)
+        }
+   
+   
+   
+    }
+
+   
 }
