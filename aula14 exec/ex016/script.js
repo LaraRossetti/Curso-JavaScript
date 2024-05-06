339de5e853fd0c0b8a78143c9bc260851e93ab73
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-06 16:20:42 -0300

    Testa a funcionalidade de somar o início e o fim

--- aula14 exec/ex016/script.js
+++ aula14 exec/ex016/script.js
@@ -1,3 +1,17 @@
 function Contar(){
-   
+   var inicio = window.document.getElementById('iníciotxt')
+   var fim = window.document.getElementById('fimtxt')
+   var passo = window.document.getElementById('passotxt')
+   var frase = window.document.querySelector('div#frase')
+
+
+   var inicio = Number(inicio.value)
+   var fim = Number(fim.value)
+   var passo = Number(passo.value)
+
+
+    var resultado = inicio + fim
+    frase.innerHTML = `Esse é o resultado: ${resultado}.`
+
+
 }
