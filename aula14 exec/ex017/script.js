c2c5910cebd60c126b816cf96646afb37b0c2b23
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-12 13:41:49 -0300

    Adiciona os itens iniciais para fazer a tabuada

---
+++ aula14 exec/ex017/script.js
@@ -0,0 +1,9 @@
+function Tabuada(){
+
+    var numero = window.document.getElementById('numerotxt')
+    var tabuada = window.document.getElementById('selecttabuada')
+    
+    var numero = Number(numero.value)
+
+
+}
