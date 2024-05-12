f1f5acf83abfbb54646f7a25706c795e5a3bafc7
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-12 14:28:08 -0300

    Apresenta erro quando o número da tabuada não é inserido

--- aula14 exec/ex017/script.js
+++ aula14 exec/ex017/script.js
@@ -2,8 +2,14 @@ function Tabuada(){
 
     var numero = window.document.getElementById('numerotxt')
     var tabuada = window.document.getElementById('selecttabuada')
+
+
+    if(numero.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        var numero = Number(numero.value)
+    }
     
-    var numero = Number(numero.value)
 
 
 }
