c16e1c78f51aa55d20605ec44384036c448c88bc
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-17 22:33:38 -0300

    Mostra a média, a soma e a quantidade números na lista

--- aula16 exec/ex018/script.js
+++ aula16 exec/ex018/script.js
@@ -1,9 +1,7 @@
-
-
 var numero = window.document.getElementById('numerotxt')
 var analisador = window.document.getElementById('selectanalisador')
 let resposta = window.document.getElementById('resposta')
-var valores = []
+let valores = []
 
 
 function ehnumero(n){
@@ -32,13 +30,41 @@ function Adicionar(){
         var item = window.document.createElement('option')
         item.text = `O número ${numero.value} foi adicionado.`
         analisador.appendChild(item) 
-        valores.push(numero.value)
+        valores.push(Number(numero.value))
     }else{
-        window.alert('nao')
+        window.alert('Não foi possível adicionar na lista, tente novamente!')
     }
     
 }
 
 function Finalizar(){
 
+    resposta.innerText = `Foram adicionados ${valores.length} números à lista.`
+    resposta.innerHTML += `<p>A soma dos valores na lista é ${soma(valores)}.</p>`
+    resposta.innerHTML += `<p>A média dos valores na lista é ${media(valores)}</p>.` 
+}
+
+function maiornumero(){
+
+}
+
+function menornumero(){
+
+}
+
+function media(v){
+    var media
+    media = soma(v) / v.length
+    return media
+}
+
+function soma(v){
+
+    var soma = 0
+    for(var posição = 0; posição < v.length; posição++){
+        var temporaria
+        temporaria = v[posição]
+        soma = soma + temporaria 
+    }
+    return soma 
 }
