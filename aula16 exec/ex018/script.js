b134097307c64d02b45fda797512893a8f7df16e
Lara Rossetti <lara.rossetti25@gmail.com>
2024-05-17 21:36:07 -0300

    Adiciona o número na lista se ele cumprir todos os requisitos

--- aula16 exec/ex018/script.js
+++ aula16 exec/ex018/script.js
@@ -1,7 +1,44 @@
+
+
+var numero = window.document.getElementById('numerotxt')
+var analisador = window.document.getElementById('selectanalisador')
+let resposta = window.document.getElementById('resposta')
+var valores = []
+
+
+function ehnumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function tanalista(n,v){
+    for(var posição = 0; posição < v.length; posição++){
+        if(n == v[posição]){
+            return true
+        }
+    }
+    return false
+}
+
+
 function Adicionar(){
 
+
+    if(ehnumero(numero.value) && !tanalista(numero.value, valores)){
+       
+        var item = window.document.createElement('option')
+        item.text = `O número ${numero.value} foi adicionado.`
+        analisador.appendChild(item) 
+        valores.push(numero.value)
+    }else{
+        window.alert('nao')
+    }
+    
 }
 
 function Finalizar(){
-    
+
 }
